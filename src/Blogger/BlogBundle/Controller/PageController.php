7d3ab65f54c8729d169b80b0feb9b09af1eb6a0d
forz13 <forz666@yandex.ru>
2017-03-11 21:51:54 +0400

    contact action  fix

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -73,12 +73,10 @@ class PageController extends Controller
             $form->handleRequest($request);
             if ($form->isValid()) {
                 $message = \Swift_Message::newInstance()
-                    ->setSubject('Contact enquiry from symblog')
-                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setSubject($enquiry->getSubject())
+                    ->setFrom($enquiry->getEmail())
                     ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
                     ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
-
-
                 $this->get('mailer')->send($message);
 
                 $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
@@ -90,7 +88,6 @@ class PageController extends Controller
             }
 
         }
-
         return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
             'form' => $form->createView()
         ));
